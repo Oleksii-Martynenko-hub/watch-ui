ab2e7c234689851c25d147597d23a75026dc8fd9
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2023-06-29 14:54:33 +0300

    Rename project

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  cacheDir: './node_modules/.vite/org',
+  cacheDir: './node_modules/.vite/watch-ui',
 
   server: {
     port: 4200,
